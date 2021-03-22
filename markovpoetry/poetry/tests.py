9954246c7b59c9e7561a0ce852e12b46d67f8faf
Sam Duthie <samduthie@gmail.com>
2021-03-22 17:04:58 +0000

    fix sentence generation test

--- markovpoetry/poetry/tests.py
+++ markovpoetry/poetry/tests.py
@@ -6,4 +6,4 @@ from poetry.poetry import generate_sentence
 class TestPoetrySentenceGeneration(TestCase):
     def test_sentence_contains_more_than_one_character(self):
         sentence = generate_sentence()
-        self.assertTrue(len(sentence > 1))
+        self.assertTrue(len(sentence) > 1)
